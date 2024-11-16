696b310b8c72ef7a830aa97fa744b9b2330e9dc5
ManeelLassoued <ing.manellassoued@gmail.com>
2024-11-16 18:39:13 +0100

    Update task2.py

--- task2.py
+++ task2.py
@@ -2,14 +2,16 @@
 # How to accept integer inputs In the previous problem,
 # we saw that input()input() assumes the input to be of type stringstring.
 # Task
-# Let us define a variable NN that accepts integer inputs.
-# Output the same variable NN.
+# Let us define a variable N
+#N that accepts integer inputs.
+# Output the same variable N.
 # To convert the input into an integer
 # integer, we use the int()int() function.Sample 1:
 # Input:
 # 4312Output:
 # 4312Code:
-# # Update the blanks in the code belowN = int(___)
+# # Update the blanks in the code below
+#N = int(___)
 # print(_)
 
 #Now write your solution from here:
